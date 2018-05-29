43afbc61fba542a37a929f9d99ab413f41351665
Aslam Chagani <aslam@codepainters.net>
2018-05-28 22:09:18 -0700

    initial commit

---
+++ R/filefinder.R
@@ -0,0 +1,56 @@
+library(shiny)
+library(miniUI)
+
+
+#' Find Files In Project
+#'
+#' use grep to find and open files
+#'
+#' @export
+fileSearch <- function() {
+
+  ui <- miniPage(
+    gadgetTitleBar("File Finder"),
+    miniContentPanel(
+      tags$div('please note that this is a development release and not ready for public use', class = "warning"),
+      tags$div("--"),
+      textInput("filename", label = "FileName", placeholder = "Enter Filename"),
+      uiOutput("files")
+    ),
+    theme = "style.css"
+  )
+
+  server <- function(input, output, session) {
+
+    obs <- list()
+
+    output$files <- renderUI({
+      files <- list.files(path = rstudioapi::getActiveProject(), pattern = input$filename, recursive = TRUE, full.names = TRUE, ignore.case = TRUE)
+      if (input$filename != "") {
+        fileWidgets <- lapply(files, function(f) {
+          if (is.null(obs[[f]])) {
+            obs[[f]] <<- observeEvent(input[[f]], {
+              rstudioapi::navigateToFile(f, line = -1L, column = -1L)
+              stopApp()
+            })
+          }
+          actionButton(inputId = f, label = f)
+        })
+
+        print(fileWidgets)
+        print(class(fileWidgets))
+        tagList(fileWidgets)
+      } else {
+        "Please Enter A Search"
+      }
+    })
+
+  }
+
+
+  viewer <- dialogViewer("Search File")
+  runGadget(ui, server, viewer = viewer)
+
+}
+
+# fileSearch()
